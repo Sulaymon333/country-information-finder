5e3a7778b0c058ddaf70e42d38ffc4640a6ae308
sulaymon333 <tjsulaymon@gmail.com>
2019-03-27 02:04:15 +0200

    developed country information finder app  beta version

--- js/index.js
+++ js/index.js
@@ -1,128 +1,134 @@
 // DOM elements
-const input = document.querySelector('#input');
+const inputField = document.querySelector('#input');
 const actions = document.querySelector('.actions');
-const startsWithButton = document.querySelector('#starts');
-const containsButton = document.querySelector('#contains');
-const sort = document.querySelector('#sort');
+const sortByNameButton = document.querySelector('#name');
+const sortByCapitalButton = document.querySelector('#capital');
+const sortByPopulationButton = document.querySelector('#population');
 const countriesTotal = document.querySelector('.total-countries');
 let countriesContainer = document.querySelector('.countries-container');
 
-/* country divs have
- div - flag
- h1  - name
- h2 - capital
- p - language
- p - population
-*/
+/* ShowCountries() function */
+const showCountries = (arr) => {
+  arr.forEach((country) => {
+    let {
+      name,
+      capital,
+      languages,
+      flag,
+      population
+    } = country
+    const countryDiv = document.createElement('div');
+    countryDiv.classList.add('country');
+    countryDiv.innerHTML += `<div><img src="${flag}"/></div>`;
+    countryDiv.innerHTML += `<h1>${name}</h1>`;
+    countryDiv.innerHTML += `<p><span>Capital: <b>${capital}</b></p>`;
+    countryDiv.innerHTML += `<p>Languages: <b>${languages.join(', ')}</b></p>`;
+    countryDiv.innerHTML += `<p>Population: <b>${String(population)}</b></h1>`;
+    countryDiv.style.color = 'red';
+    countryDiv.style
+      .fontWeight = '500'
+    countryDiv.style.backgroundColor = `rgba(255, 255, 255, 0.7)`;
+    countriesContainer.appendChild(countryDiv);
+  })
+  // Get the total number of countries and style it - default
+  countriesTotal.innerHTML = arr.length;
+  countriesTotal.style.fontWeight = '700';
+}
+showCountries(countries)
 
-// build country DIV's
-function countryDivs(name, capital, languages, population, flag) {
-  const countryDiv = document.createElement('div');
-  countryDiv.classList.add('country');
-  countryDiv.innerHTML += `<div><img src="${flag}"/></div>`;
-  countryDiv.innerHTML += `<h1>${name}</h1>`;
-  countryDiv.innerHTML += `<p><span>Capital: <b>${capital}</b></p>`;
-  countryDiv.innerHTML += `<p>Languages: <b>${languages.join(', ')}</b></p>`;
-  countryDiv.innerHTML += `<p>Population: <b>${String(population)}</b></h1>`;
-  countryDiv.style.color = 'red';
-  countryDiv.style
-    .fontWeight = '500'
-  countryDiv.style.backgroundColor = `rgba(255, 255, 255, 0.7)`;
-  countriesContainer.appendChild(countryDiv);
+let [
+  name,
+  capital,
+  languages,
+  population,
+  currency
+] = countries
+
+/*=== sortCountriesByName() function ===*/
+const filterCountriesBySearchTerm = (arr, searchTerm) => {
+  let filtered = arr.filter((country) => {
+    let {
+      name,
+      capital,
+      languages
+    } = country
+    let match = name.toLowerCase().includes(searchTerm.toLowerCase()) || capital.toLowerCase().includes(searchTerm.toLowerCase()) || languages.join(',').toLowerCase().includes(searchTerm.toLowerCase())
+    return match
+  })
+  // console.log(filtered);
+  let result = searchTerm == '' ? arr : filtered
+  return result;
 }
 
-// Get ech country data
-countries.forEach((country) => {
-  let {
-    name,
-    capital,
-    languages,
-    population,
-    currency
-  } = country
+/*=== EVENT LISTENERS ===*/
+/*=== An event listener for sort by name button ===*/
+inputField.addEventListener('input', (e) => {
+  countriesContainer.innerHTML = '';
+  const searchTerm = e.target.value;
+  showCountries(filterCountriesBySearchTerm(countries, searchTerm))
+
 })
-let w = '';
-console.log(String(name.population).split('').forEach((item, index) => {
-  console.log(item)
-
-  if ((index + 2) % 3 == 0) {
-
-    w += item
-  }
-  console.log(w);
-}));
-
-
-// Button Switcher function
-startsWithButton.classList.add('selected-button');
-
-function buttonSwitcher(button1, button2) {
-  button2.addEventListener('click', function () {
-    button2.classList.add('selected-button');
-    button1.classList.remove('selected-button');
-  });
-}
-buttonSwitcher(startsWithButton, containsButton) // starts with
-buttonSwitcher(containsButton, startsWithButton) // contains
-
-// capitalize input
-function capitalize(value) {
-  let firstItem = value.slice(0, 1).toUpperCase();
-  return (value = firstItem + value.slice(1));
-}
-
-// Get the total number of countries and style it - default
-countriesTotal.innerHTML = countries.length;
-countriesTotal.style.fontWeight = '700';
-
-// Show all the countries - default view
-countries.forEach(country => {
-  // Get ech country data
-  let {
-    name,
-    capital,
-    languages,
-    population,
-    flag
-  } = country
-  countryDivs(name, capital, languages, population, flag);
-});
-
-// initialize sort type arrays
-let startsWith;
-let contains;
-
-input.addEventListener('keyup', function (e) {
-  // contains = countries.filter(country => {
-  //   return country.toLowerCase().includes((e.target.value.toLowerCase()));
-  // });
-  sortType(contains)
-  containsButton.addEventListener('click', containsChecker);
-
-  startsWith = countries.filter(country => {
-    countriesContainer.innerHTML = '';
-    return country.startsWith(capitalize(e.target.value));
-  });
-  sortType(startsWith)
-  startsWithButton.addEventListener('click', startsWithChecker);
-});
-
-// starts with function
-function startsWithChecker() {
+/*=== An event listener for sort by name button ===*/
+sortByNameButton.addEventListener('click', () => {
+  sortByNameButton.classList.add('selected-button')
+  const otherButtons = buttonSiblings(sortByNameButton)
+  otherButtons.forEach((button) => {
+    if (button.classList.contains('selected-button')) {
+      button.classList.remove('selected-button')
+    }
+  })
   countriesContainer.innerHTML = '';
-  sortType(startsWith);
-}
-
-// contains function
-function containsChecker() {
+  showCountries(filterCountriesBySearchTerm(countries, inputField.value).sort((a, b) => {
+    if (a.name < b.name) {
+      return 1
+    }
+    if (a.name > b.name) {
+      return -1
+    }
+    return 0
+  }))
+})
+/*=== An event listener for sort by capital button ===*/
+sortByCapitalButton.addEventListener('click', () => {
+  sortByCapitalButton.classList.add('selected-button')
+  const otherButtons = buttonSiblings(sortByCapitalButton)
+  otherButtons.forEach((button) => {
+    if (button.classList.contains('selected-button')) {
+      button.classList.remove('selected-button')
+    }
+  })
   countriesContainer.innerHTML = '';
-  sortType(contains)
-}
+  showCountries(filterCountriesBySearchTerm(countries, inputField.value).sort((a, b) => {
+    if (a.capital < b.capital) {
+      return -1
+    }
+    if (a.capital > b.capital) {
+      return 1
+    }
+    return 0
+  }))
+})
+/*=== An event listener for sort by population button ===*/
+sortByPopulationButton.addEventListener('click', () => {
+  sortByPopulationButton.classList.add('selected-button')
+  const otherButtons = buttonSiblings(sortByPopulationButton)
+  otherButtons.forEach((button) => {
+    if (button.classList.contains('selected-button')) {
+      button.classList.remove('selected-button')
+    }
+  })
 
-// get sorted data without clicking buttons
-function sortType(sortType) {
-  sortType.forEach(countrySorted => {
-    countryDivs(countrySorted)
-  });
-  countriesTotal.innerHTML = sortType.length;
-}
+  countriesContainer.innerHTML = '';
+  showCountries(filterCountriesBySearchTerm(countries, inputField.value).sort((a, b) => {
+    if (a.population < b.population) {
+      return 1
+    }
+    if (a.population > b.population) {
+      return -1
+    }
+    return 0
+  }))
+})
+
+/* ButtonChanger() */
+const buttonSiblings = n => [...n.parentElement.children].filter(currentButton => currentButton.nodeType === 1 && currentButton != n)
